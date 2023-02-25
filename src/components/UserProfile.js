0ed7a53864abc5d2471ab5dce81b416e99ecc3ca
deepak104080 <deepak104080@gmail.com>
2023-02-25 22:03:11 +0530

    Assg Context

--- src/components/UserProfile.js
+++ src/components/UserProfile.js
@@ -4,6 +4,15 @@ import { UserContext } from "./App"
 
 const UserProfile = () =>{
 
+    let localContext = useContext(UserContext);
+
+    return(
+        <>
+            <h1 id="name">{localContext.name}</h1>
+            <h1 id="age">{localContext.age}</h1>
+        </>
+    )
+
 }
 
 export {UserProfile}
